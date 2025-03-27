b88a191af479b025b2a7a3d1653e371eb18aa9fe
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-27 15:56:51 +0000

    Styled menu section

--- web/core/js/script.js
+++ web/core/js/script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("Document has been loaded");
-
     let mainSection = document.getElementsByClassName("main-section");
-    
+
 });
