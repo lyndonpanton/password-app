4071f4982546b1d0be85005d9d9527edd439dfd6
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-27 15:33:00 +0000

    Added menu section

---
+++ web/core/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("Document has been loaded");
+
+    let mainSection = document.getElementsByClassName("main-section");
+    
+});
