830197a7b510a16acd5654420d90ade56fbe77f1
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-27 16:06:48 +0000

    Users can toggle between app and menu

--- web/core/js/script.js
+++ web/core/js/script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    let menuToggle = document.getElementsByClassName("menu-toggle");
     let mainSection = document.getElementsByClassName("main-section");
+    let header = document.getElementsByTagName("header")[0];
+    let menu = document.getElementById("menu");
+    let app = document.getElementById("app");
 
+    for (let i = 0; i < menuToggle.length; i++) {
+        menuToggle[i].addEventListener("click", toggleMenu);
+    }
+
+    function toggleMenu(event) {
+        if (event.target.id == "menu-open") {
+            // hide header, app
+            header.classList.add("hidden");
+            app.classList.add("hidden");
+            // show menu
+            menu.classList.remove("hidden");
+        } else {
+            // hide menu
+            menu.classList.add("hidden");
+            // show header, app
+            header.classList.remove("hidden");
+            app.classList.remove("hidden");
+        }
+    }
 });
