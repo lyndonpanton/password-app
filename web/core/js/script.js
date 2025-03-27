7e6d5d5fd5b188265936c0375a340d3d6aef1f80
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-27 16:17:10 +0000

    User can toggle between application sections

--- web/core/js/script.js
+++ web/core/js/script.js
@@ -1,12 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let menuToggle = document.getElementsByClassName("menu-toggle");
-    let mainSection = document.getElementsByClassName("main-section");
+    let menuListItem = document.getElementsByClassName("menu-list-item");
     let header = document.getElementsByTagName("header")[0];
     let menu = document.getElementById("menu");
     let app = document.getElementById("app");
+    let appSection = document.getElementsByClassName("main-feature");
 
     for (let i = 0; i < menuToggle.length; i++) {
         menuToggle[i].addEventListener("click", toggleMenu);
+    }
+
+    for (let i = 0; i < menuListItem.length; i++) {
+        menuListItem[i].addEventListener("click", changeAppSection);
+    }
+
+    function changeAppSection(event) {
+        let sectionName = event.target.id.slice(
+            event.target.id.lastIndexOf("-") + 1
+        );
+
+        for (let i = 0; i < appSection.length; i++) {
+            if (appSection[i].id == sectionName) {
+                appSection[i].classList.remove("hidden");
+            } else {
+                appSection[i].classList.add("hidden");
+            }
+        }
     }
 
     function toggleMenu(event) {
